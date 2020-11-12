07bdd0c449a1fa6dcb922c3bc5b6c9f340effac3
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-11-12 11:35:37 +0100

    Add Product.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import Product from './Product';
 import './App.css';
 
 function App() {
   return (
     <main>
-      <article className="product">
-        <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" alt="Citroenen"/>
-        <h2 className="product-name">Citroen</h2>
-        <p className="product-description">
-          Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees,
-          het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel.
-        </p>
-      </article>
+      <Product
+        image="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg"
+        title="Citroen"
+        description="Een citroen is voor de meeste mensen te zuur om zo uit de hand te eten. Van citroen kun je het vruchtvlees, het sap en de schil gebruiken. Het sappige, lichtgele zure vruchtvlees versterkt de smaak van ander voedsel."
+      />
       <article className="product">
         <img src="https://www.plusonline.nl/sites/plusonline/files/citroen_1.jpg" alt="Citroenen"/>
         <h2 className="product-name">Limoen</h2>
